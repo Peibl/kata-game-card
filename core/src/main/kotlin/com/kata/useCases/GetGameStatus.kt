d1ccc334cfb33d41310e3487ab8561daa9095851
Edgar <edgargmc@gmail.com>
2020-09-04 18:39:30 -0300

    Refactor - fix test

--- core/src/main/kotlin/com/kata/useCases/GetGameStatus.kt
+++ core/src/main/kotlin/com/kata/useCases/GetGameStatus.kt
@@ -3,7 +3,7 @@ package com.kata.useCases
 import com.kata.domain.GameStatus
 import com.kata.domain.CurrentGame
 
-class GetGameStatus(val games: CurrentGame) {
+class GetGameStatus(private val games: CurrentGame) {
     fun execute(): GameStatus {
         return games.current!!.getStatus()
     }
